cb84349f6333d55d688ac7e5a347e93f126dd372
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-02-04 08:41:01 +0000

    Testing connection to DB
    Demo

---
+++ src/com/demo/jdbc/TestJdbc.java
@@ -0,0 +1,29 @@
+package com.demo.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+
+	public static void main(String[] args) {
+		
+		String jdbcurl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false";
+		String user = "hbstudent";
+		String pass = "hbstudent";
+		try {
+			
+			System.out.println("Connection to database: "+jdbcurl);
+			
+			Connection myConn = 
+					DriverManager.getConnection(jdbcurl, user, pass);
+			
+			System.out.println("Connection Successful!!!");
+			
+			
+		}catch (Exception exc) {
+			exc.printStackTrace();
+		}
+
+	}
+
+}
